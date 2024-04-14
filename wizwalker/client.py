196241650af8e25c4ea278420b8397850e9a5dd2
SirOlaf <34164198+SirOlaf@users.noreply.github.com>
2024-04-14 12:16:50 +0200

    Patch goto for pet mode (#56)
    * Patch goto for pet mode
    
    * Fix name

--- wizwalker/client.py
+++ wizwalker/client.py
@@ -429,15 +429,17 @@ class Client:
             x: X to move to
             y: Y to move to
         """
-        current_xyz = await self.body.position()
+        client_obj = self.client_object
+        body = await client_obj.actor_body()
+        current_xyz = await body.position()
         # (40 / 100) + 1 = 1.4
-        speed_multiplier = ((await self.client_object.speed_multiplier()) / 100) + 1
+        speed_multiplier = ((await client_obj.speed_multiplier()) / 100) + 1
         target_xyz = utils.XYZ(x, y, current_xyz.z)
         distance = current_xyz - target_xyz
         move_seconds = distance / (WIZARD_SPEED * speed_multiplier)
         yaw = utils.calculate_perfect_yaw(current_xyz, target_xyz)
 
-        await self.body.write_yaw(yaw)
+        await body.write_yaw(yaw)
         await utils.timed_send_key(self.window_handle, Keycode.W, move_seconds)
 
     async def teleport(
