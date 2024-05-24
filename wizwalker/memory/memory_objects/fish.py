d839e96314431c6c9c224801a32e089183c6ca05
SirOlaf <sirolaf@protonmail.com>
2024-05-25 01:12:21 +0200

    Add chest marker

--- wizwalker/memory/memory_objects/fish.py
+++ wizwalker/memory/memory_objects/fish.py
@@ -7,16 +7,19 @@ class Fish(DynamicMemoryObject):
     async def body(self) -> DynamicActorBody:
         addr = await self.read_value_from_offset(0x48, "unsigned long long")
         return DynamicActorBody(self.hook_handler, addr)
-    
+
     async def template(self) -> FishTemplate:
         addr = await self.read_value_from_offset(0xD8, "unsigned long long")
         return FishTemplate(self.hook_handler, addr)
 
     async def fish_id(self) -> int:
         return await self.read_value_from_offset(0xE4, "int")
-    
+
     async def template_id(self) -> int:
         return await self.read_value_from_offset(0xE8, "int")
-    
+
     async def size(self) -> float:
         return await self.read_value_from_offset(0xEC, "float")
+
+    async def is_chest(self) -> bool:
+        return await self.read_value_from_offset(0xF0, "bool")
